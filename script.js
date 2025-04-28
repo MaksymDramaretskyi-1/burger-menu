838f460ea8de524a78cec501afee488f59b640e5
MaksymDramaretskyi-1 <maksym.glodedata@gmail.com>
2025-04-28 14:00:39 +0200

    firstCommit

--- script.js
+++ script.js
@@ -9,10 +9,32 @@
 //const
 //var
 
-let userName = 'Maksym';
-console.log(typeof userName);
+// let userName = 'Maksym';
+// console.log(typeof userName);
 
 
-let userAge = 26;
+// let userAge = 26;
 
-let isPresent = true;
+// let isPresent = true;
+
+// function infoText () {
+//   const firstName = 'Maksym';
+//   const lastName = 'Dramaretskyi';
+
+//   console.log(` ${firstName} ${lastName}`)
+
+//   const main = document.querySelector('main');
+//   main.textContent = `${firstName} ${lastName}`;
+// }
+
+// infoText();
+
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav");
+  
+    burger.addEventListener("click", () => {
+      burger.classList.toggle("open");
+      nav.classList.toggle("active");
+    });
+  });  
