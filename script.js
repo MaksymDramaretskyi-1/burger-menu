3af2107d8eebe3f49d7977a7125fe6ac80a7130e
MaksymDramaretskyi-1 <maksym.glodedata@gmail.com>
2025-04-28 10:43:56 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// DATATYPES
+// Primative datatypes
+//string
+// number
+// boolean
+
+//VARAIBLES
+// let
+//const
+//var
+
+let userName = 'Maksym';
+console.log(typeof userName);
+
+
+let userAge = 26;
+
+let isPresent = true;
